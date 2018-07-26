7c3e90ebd755111f2812952b897d433fef4241b6
skc1983 <33701890+skc1983@users.noreply.github.com>
2018-07-26 19:55:13 +0700

    Update add-new-wifi-linux-cmd.c

--- add-new-wifi-linux-cmd.c
+++ add-new-wifi-linux-cmd.c
@@ -9,7 +9,29 @@ int main(int argc,char **argv){
 	char c[]="\"\n}\n";
 	fp=fopen("/etc/wpa_supplicant/wpa_supplicant.conf","a+");
 	fseek(fp,0,SEEK_END);
-	if(argc==4){					    //network name with a space e.g. A hostel
+	if(argc==6){						//network name with a space e.g. A a a hostel
+		fwrite(a,sizeof a-1,1,fp);
+		fwrite(argv[1],strlen(argv[1]),1,fp);
+		fwrite(" ",1,1,fp);
+		fwrite(argv[2],strlen(argv[2]),1,fp);
+		fwrite(" ",1,1,fp);
+		fwrite(argv[3],strlen(argv[3]),1,fp);
+		fwrite(" ",1,1,fp);
+		fwrite(argv[4],strlen(argv[4]),1,fp);
+		fwrite(b,sizeof b-1,1,fp);
+		fwrite(argv[5],strlen(argv[5]),1,fp);
+		fwrite(c,sizeof c-1,1,fp);
+	}else if(argc==5){					//network name with a space e.g. A a hostel
+		fwrite(a,sizeof a-1,1,fp);
+		fwrite(argv[1],strlen(argv[1]),1,fp);
+		fwrite(" ",1,1,fp);
+		fwrite(argv[2],strlen(argv[2]),1,fp);
+		fwrite(" ",1,1,fp);
+		fwrite(argv[3],strlen(argv[3]),1,fp);
+		fwrite(b,sizeof b-1,1,fp);
+		fwrite(argv[4],strlen(argv[4]),1,fp);
+		fwrite(c,sizeof c-1,1,fp);
+	}else if(argc==4){					//network name e.g. A_hostel
 		fwrite(a,sizeof a-1,1,fp);
 		fwrite(argv[1],strlen(argv[1]),1,fp);
 		fwrite(" ",1,1,fp);
@@ -17,7 +39,7 @@ int main(int argc,char **argv){
 		fwrite(b,sizeof b-1,1,fp);
 		fwrite(argv[3],strlen(argv[3]),1,fp);
 		fwrite(c,sizeof c-1,1,fp);
-	}else if(argc==3){				//network name e.g. A_hostel
+	}else if(argc==3){					//network name e.g. A_hostel
 		fwrite(a,sizeof a-1,1,fp);
 		fwrite(argv[1],strlen(argv[1]),1,fp);
 		fwrite(b,sizeof b-1,1,fp);
